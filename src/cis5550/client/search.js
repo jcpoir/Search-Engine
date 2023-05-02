def3a42391692e559e4c5f80d640af4ea4eab6d5
kaihao liu <yubanmeiqin9908@gmail.com>
2023-05-02 14:39:27 -0400

    category search and readme

--- src/cis5550/client/search.js
+++ src/cis5550/client/search.js
@@ -1,4 +1,19 @@
-async function search(event, page = 1) {
+async function categoryClicked(event, category) {
+  await search(event, 1, true, category);
+}
+
+async function search(event, page = 1, isCategory = false, category = "") {
+  var searchValue;
+
+  // check if category link is clicked
+  // if som search value = category value
+  if (isCategory) {
+    searchValue = category;
+  } else {
+    // if its a normal search
+    // Get the user's search query from the form input field
+    searchValue = document.getElementById("query").value;
+  }
   event.preventDefault();
 
   // Clear any existing search results from the page
@@ -6,10 +21,6 @@ async function search(event, page = 1) {
   while (searchResults.firstChild) {
     searchResults.removeChild(searchResults.firstChild);
   }
-
-  // Get the user's search query from the form input field
-  const searchValue = document.getElementById("query").value;
-  console.log(`Search query: ${searchValue}`);
 
   try {
     const itemsPerPage = 10;
@@ -29,6 +40,7 @@ async function search(event, page = 1) {
 
     // const data = response.data;
 
+    // start for dummy generateion, remove after url inplemented
     let data;
     if (searchValue == "2") {
       data = dummyData2;
@@ -36,12 +48,19 @@ async function search(event, page = 1) {
       data = dummyData3;
     } else if (searchValue == "long") {
       data = dummyDataLong;
-    }
-      else {
+    } else if (searchValue == "books") {
+      data = dummyDataBooks;
+    } else if (searchValue == "music") {
+      data = dummyDataMusic;
+    } else if (searchValue == "sports") {
+      data = dummyDataSports;
+    } else if (searchValue == "travel") {
+      data = dummyDataTravel;
+    } else {
       data = dummyData1;
     }
+    // end for dummy generateion
 
-    // console.log('results', data, startIndex, endIndex)
     const results = data.slice(startIndex, endIndex);
     const totalPages = Math.ceil(data.length / itemsPerPage);
 
@@ -50,7 +69,6 @@ async function search(event, page = 1) {
 
     // Loop through each search result and create a new list item for it
     results.forEach((result) => {
-      console.log('result',result)
       // Create a new list item element
       const listItem = document.createElement("li");
       listItem.classList.add("search-result");
@@ -100,39 +118,70 @@ async function search(event, page = 1) {
   }
 }
 
-
 const dummyDataLong = Array.from({ length: 200 }, (_, i) => {
   const page = Math.floor(i / 10) + 1;
-  return { url: `https://example.com/result/${i + 1}`, page: `This is a dummy description for the result item ${page}` };
+  return {
+    url: `https://example.com/result/${i + 1}`,
+    page: `This is a dummy description for the result item ${page}`,
+  };
 });
 
+const dummyData1 = [];
+for (let i = 1; i <= 5; i++) {
+  const data = {
+    url: `https://example.com/search/${i}`,
+    page: `Here's a short description of the search result ${i}.`
+  };
+  dummyData1.push(data);
+}
 
+const dummyData2 = [];
+for (let i = 1; i <= 5; i++) {
+  const data = {
+    url: `https://example.com/articles/${i}`,
+    page: `This is a placeholder text for article ${i}.`
+  };
+  dummyData2.push(data);
+}
 
-const dummyData1 = [
-  { url: "https://example.com/search/1", page: "Here's a short description of the first search result." },
-  { url: "https://example.com/search/2", page: "Here's a short description of the second search result." },
-  { url: "https://example.com/search/3", page: "Here's a short description of the third search result." },
-  { url: "https://example.com/search/4", page: "Here's a short description of the fourth search result." },
-  { url: "https://example.com/search/5", page: "Here's a short description of the fifth search result." },
-];
+const dummyData3 = [];
+for (let i = 1; i <= 5; i++) {
+  const data = {
+    url: `https://example.com/guides/${i}`,
+    page: `This is a placeholder text for guide ${i}.`
+  };
+  dummyData3.push(data);
+}
 
-const dummyData2 = [
-  { url: "https://example.com/articles/1", page: "Learn how to make a delicious and satisfying cup of coffee at home, including tips for choosing the best beans and brewing methods." },
-  { url: "https://example.com/articles/2", page: "Discover the many ways that practicing yoga can improve your physical and mental health, including increased flexibility, strength, and relaxation." },
-  { url: "https://example.com/articles/3", page: "Get inspired for your next meal with these simple and nutritious dinner ideas, perfect for busy weeknights or lazy weekends." },
-  { url: "https://example.com/articles/4", page: "Explore the most popular sights and experiences in the City of Light, from the iconic Eiffel Tower to the charming streets of Montmartre." },
-  { url: "https://example.com/articles/5", page: "Learn the essential steps for launching and growing a profitable online business, including strategies for marketing, sales, and customer service." },
-];
+const dummyDataBooks = [];
+for (let i = 1; i <= 5; i++) {
+  const data = {
+    url: `https://example.com/books/${i}`,
+    page: `This is a placeholder text for book ${i}.`
+  };
+  dummyDataBooks.push(data);
+}
 
-const dummyData3 = [
-  { url: "https://example.com/guides/1", page: "Discover the most anticipated books of the year, from bestselling authors to up-and-coming debut novelists. Get ready to add these titles to your reading list!" },
-  { url: "https://example.com/guides/2", page: "Are you a thrill-seeker looking for your next adventure? Check out these top travel destinations, perfect for hiking, biking, kayaking, and more." },
-  { url: "https://example.com/guides/3", page: "Simplify your closet and elevate your style with a capsule wardrobe. Learn how to choose versatile pieces that can be mixed and matched for any occasion." },
-  { url: "https://example.com/guides/4", page: "Are you struggling to balance your work and personal life? Check out these time management tips and tricks for boosting your productivity and achieving your goals." },
-  { url: "https://example.com/guides/5", page: "Discover the science-backed benefits of meditation for reducing stress and anxiety, improving focus and concentration, and promoting overall well-being." },
-];
+const dummyDataMusic = [];
+for (let i = 1; i <= 5; i++) {
+  dummyDataMusic.push({
+    url: `https://example.com/music/${i}`,
+    page: `This is a placeholder text for music ${i}.`,
+  });
+}
 
-  
-//   // Update your form event listener to call the search function with the initial page number
-// const form = document.getElementById("search-form");
-// form.addEventListener("submit", (event) => search(event, 1));
+const dummyDataSports = [];
+for (let i = 1; i <= 5; i++) {
+  dummyDataSports.push({
+    url: `https://example.com/sports/${i}`,
+    page: `This is a placeholder text for sports ${i}.`,
+  });
+}
+
+const dummyDataTravel = [];
+for (let i = 1; i <= 5; i++) {
+  dummyDataTravel.push({
+    url: `https://example.com/travel/${i}`,
+    page: `This is a placeholder text for travel ${i}.`,
+  });
+}
