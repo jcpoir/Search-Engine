368d2f0d6cc7796e0f692bc2333cb4bec3357aa2
kaihao liu <yubanmeiqin9908@gmail.com>
2023-05-02 14:10:32 -0400

    format change to array of dic

--- src/cis5550/client/search.js
+++ src/cis5550/client/search.js
@@ -16,6 +16,7 @@ async function search(event, page = 1) {
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
 
+    // TODO: replace with correct url
     // // Replace the URL with your Java server's URL
     // const serverUrl = 'https://your-java-server-url.com';
 
@@ -40,14 +41,16 @@ async function search(event, page = 1) {
       data = dummyData1;
     }
 
-    const results = data.results.slice(startIndex, endIndex);
-    const totalPages = Math.ceil(data.results.length / itemsPerPage);
+    // console.log('results', data, startIndex, endIndex)
+    const results = data.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(data.length / itemsPerPage);
 
     // Create a new unordered list to hold the search results
     const resultList = document.createElement("ul");
 
     // Loop through each search result and create a new list item for it
     results.forEach((result) => {
+      console.log('result',result)
       // Create a new list item element
       const listItem = document.createElement("li");
       listItem.classList.add("search-result");
@@ -55,13 +58,13 @@ async function search(event, page = 1) {
       // Create a new heading element for the search result title, with a link to the full article
       const title = document.createElement("h3");
       const link = document.createElement("a");
-      link.href = `/result/${result.id}`;
-      link.textContent = result.title;
+      link.href = result.url;
+      link.textContent = result.url;
       title.appendChild(link);
 
       // Create a new paragraph element for the search result description
       const description = document.createElement("p");
-      description.textContent = result.description;
+      description.textContent = result.page;
 
       // Add the title and description to the list item
       listItem.appendChild(title);
@@ -98,110 +101,37 @@ async function search(event, page = 1) {
 }
 
 
-const dummyDataLong = {
-  results: [
-    // You can use a for loop to generate 100 dummy items
-    ...Array(200).keys()].map((i) => {
-      return {
-        id: i + 1,
-        title: `Dummy Title ${i + 1}`,
-        description: `This is a dummy description for the result item ${i + 1}.`,
-      };
-    })
-};
+const dummyDataLong = Array.from({ length: 200 }, (_, i) => {
+  const page = Math.floor(i / 10) + 1;
+  return { url: `https://example.com/result/${i + 1}`, page: `This is a dummy description for the result item ${page}` };
+});
 
 
 
+const dummyData1 = [
+  { url: "https://example.com/search/1", page: "Here's a short description of the first search result." },
+  { url: "https://example.com/search/2", page: "Here's a short description of the second search result." },
+  { url: "https://example.com/search/3", page: "Here's a short description of the third search result." },
+  { url: "https://example.com/search/4", page: "Here's a short description of the fourth search result." },
+  { url: "https://example.com/search/5", page: "Here's a short description of the fifth search result." },
+];
 
+const dummyData2 = [
+  { url: "https://example.com/articles/1", page: "Learn how to make a delicious and satisfying cup of coffee at home, including tips for choosing the best beans and brewing methods." },
+  { url: "https://example.com/articles/2", page: "Discover the many ways that practicing yoga can improve your physical and mental health, including increased flexibility, strength, and relaxation." },
+  { url: "https://example.com/articles/3", page: "Get inspired for your next meal with these simple and nutritious dinner ideas, perfect for busy weeknights or lazy weekends." },
+  { url: "https://example.com/articles/4", page: "Explore the most popular sights and experiences in the City of Light, from the iconic Eiffel Tower to the charming streets of Montmartre." },
+  { url: "https://example.com/articles/5", page: "Learn the essential steps for launching and growing a profitable online business, including strategies for marketing, sales, and customer service." },
+];
 
-const dummyData1 = {results: [
-    {
-        id: 1,
-        title: "Search Result 1",
-        description: "Here's a short description of the first search result. This is a placeholder text to demonstrate what a search result might look like.",
-    },
-    {
-        id: 2,
-        title: "Search Result 2",
-        description: "Here's a short description of the second search result. This is a placeholder text to demonstrate what a search result might look like.",
-    },
-    {
-        id: 3,
-        title: "Search Result 3",
-        description: "Here's a short description of the third search result. This is a placeholder text to demonstrate what a search result might look like.",
-    },
-    {
-        id: 4,
-        title: "Search Result 4",
-        description: "Here's a short description of the fourth search result. This is a placeholder text to demonstrate what a search result might look like.",
-    },
-    {
-        id: 5,
-        title: "Search Result 5",
-        description: "Here's a short description of the fifth search result. This is a placeholder text to demonstrate what a search result might look like.",
-    },
-]};
+const dummyData3 = [
+  { url: "https://example.com/guides/1", page: "Discover the most anticipated books of the year, from bestselling authors to up-and-coming debut novelists. Get ready to add these titles to your reading list!" },
+  { url: "https://example.com/guides/2", page: "Are you a thrill-seeker looking for your next adventure? Check out these top travel destinations, perfect for hiking, biking, kayaking, and more." },
+  { url: "https://example.com/guides/3", page: "Simplify your closet and elevate your style with a capsule wardrobe. Learn how to choose versatile pieces that can be mixed and matched for any occasion." },
+  { url: "https://example.com/guides/4", page: "Are you struggling to balance your work and personal life? Check out these time management tips and tricks for boosting your productivity and achieving your goals." },
+  { url: "https://example.com/guides/5", page: "Discover the science-backed benefits of meditation for reducing stress and anxiety, improving focus and concentration, and promoting overall well-being." },
+];
 
-
-const dummyData2 = {
-    results: [
-      {
-        id: 1,
-        title: "How to Make the Perfect Cup of Coffee",
-        description: "Learn how to make a delicious and satisfying cup of coffee at home, including tips for choosing the best beans and brewing methods.",
-      },
-      {
-        id: 2,
-        title: "The Benefits of Yoga for Mind and Body",
-        description: "Discover the many ways that practicing yoga can improve your physical and mental health, including increased flexibility, strength, and relaxation.",
-      },
-      {
-        id: 3,
-        title: "10 Easy and Healthy Dinner Recipes",
-        description: "Get inspired for your next meal with these simple and nutritious dinner ideas, perfect for busy weeknights or lazy weekends.",
-      },
-      {
-        id: 4,
-        title: "The Top 5 Tourist Attractions in Paris",
-        description: "Explore the most popular sights and experiences in the City of Light, from the iconic Eiffel Tower to the charming streets of Montmartre.",
-      },
-      {
-        id: 5,
-        title: "How to Build a Successful Online Business",
-        description: "Learn the essential steps for launching and growing a profitable online business, including strategies for marketing, sales, and customer service.",
-      },
-    ],
-  };
-
-  const dummyData3 = {
-    results: [
-      {
-        id: 1,
-        title: "The Best Books of 2023: A Reading List",
-        description: "Discover the most anticipated books of the year, from bestselling authors to up-and-coming debut novelists. Get ready to add these titles to your reading list!",
-      },
-      {
-        id: 2,
-        title: "10 Must-Visit Destinations for Adventure Travel",
-        description: "Are you a thrill-seeker looking for your next adventure? Check out these top travel destinations, perfect for hiking, biking, kayaking, and more.",
-      },
-      {
-        id: 3,
-        title: "How to Create a Capsule Wardrobe for Any Occasion",
-        description: "Simplify your closet and elevate your style with a capsule wardrobe. Learn how to choose versatile pieces that can be mixed and matched for any occasion.",
-      },
-      {
-        id: 4,
-        title: "5 Tips for Better Time Management and Productivity",
-        description: "Are you struggling to balance your work and personal life? Check out these time management tips and tricks for boosting your productivity and achieving your goals.",
-      },
-      {
-        id: 5,
-        title: "The Benefits of Meditation for Stress and Anxiety",
-        description: "Discover the science-backed benefits of meditation for reducing stress and anxiety, improving focus and concentration, and promoting overall well-being.",
-      },
-    ],
-  };
   
 //   // Update your form event listener to call the search function with the initial page number
 // const form = document.getElementById("search-form");
